004f10cbeac5faafa046fcda3bd332bbea4fef40
Luciano Liboni <luciano_liboni@hotmail.com>
2020-02-19 13:11:45 -0300

    Criando loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Luciano Liboni',
+    email: 'luciano@email.com',
+    password_hash: '12343654',
+  });
+
+  return res.json(user);
+});
 export default routes;
