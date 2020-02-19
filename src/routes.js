57c0ed53758c0a9843ccb10517fdce0c51edf950
Luciano Liboni <luciano_liboni@hotmail.com>
2020-02-19 13:32:17 -0300

    Controller de usuários - cadastro

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Luciano Liboni',
-    email: 'luciano@email.com',
-    password_hash: '12343654',
-  });
+routes.post('/users', UserController.store);
 
-  return res.json(user);
-});
 export default routes;
