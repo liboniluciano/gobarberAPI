9f6211f88745c91209b565b7b271faf85b18130b
Luciano Liboni <luciano_liboni@hotmail.com>
2020-02-21 15:12:08 -0300

    Criando campos virtuais para agendamento

--- src/app/models/Appointments.js
+++ src/app/models/Appointments.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import { isBefore, subHours } from 'date-fns';
 
 class Appointments extends Model {
   static init(sequelize) {
@@ -6,6 +7,19 @@ class Appointments extends Model {
       {
         date: Sequelize.DATE,
         canceled_at: Sequelize.DATE,
+        past: {
+          type: Sequelize.VIRTUAL,
+          // Verificando se o agendamento é de uma data passada
+          get() {
+            return isBefore(this.date, new Date());
+          },
+        },
+        cancelable: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return isBefore(new Date(), subHours(this.date, 2));
+          },
+        },
       },
       {
         sequelize,
