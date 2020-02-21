9bec7475d1615e46296e6f91537e8c22840893b2
Luciano Liboni <luciano_liboni@hotmail.com>
2020-02-21 10:51:38 -0300

    Configurando Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '17491396362605',
+    pass: '6191b361a8d59f',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
